247fb09f5e903f17915349f06c9a69cf2a2e94cd
Ujjwal Aglawe <aglaweujjwal@gmail.com>
2025-10-20 21:35:12 +0530

    Adding Files 2

--- netlify/function/app.js
+++ netlify/function/app.js
@@ -349,10 +349,10 @@ app.post("/order/validate", async (req, res) => {
   }
 });
 
-app.use("/.netlify/functions/app", router);
-module.exports.handler = serverless(app);
+export const handler = serverless(app);
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
